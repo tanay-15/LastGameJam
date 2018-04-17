e2e6c0254b24c7f1336e924e4cff54dc069eb43f
Joshua Rand <jrand35@aol.com>
2018-04-16 20:36:29 -0400

    Coin counter

---
+++ Pinballtest/Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    [SerializeField] GameObject coinParticlesPrefab;
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        CoinCounter.sharedInstance.DeleteCoin();
+        Instantiate(coinParticlesPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        transform.rotation = Camera.main.gameObject.transform.rotation;
+    }
+}
---
+++ Pinballtest/Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCounter : MonoBehaviour {
+
+    public static CoinCounter sharedInstance;
+
+    int coinCount;
+    [SerializeField] TextMesh text;
+	void Start () {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        coinCount = FindObjectsOfType<Coin>().Length;
+        UpdateText();
+	}
+
+    void UpdateText()
+    {
+        text.text = "x " + coinCount.ToString();
+    }
+	
+	void Update () {
+		
+	}
+
+    public void DeleteCoin()
+    {
+        coinCount--;
+        UpdateText();
+    }
+}
