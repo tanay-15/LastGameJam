92539605abf23da4f77d46fd4f3421f2799b8408
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-04-26 01:02:53 -0400

    change button color that opens the door.

---
+++ 3DPinball/Assets/Scripts/colorChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour {
+    Renderer rend;
+    bool changeColor = false;
+    public Material finalColor;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(changeColor)
+        {
+
+            rend.sharedMaterial = finalColor;
+            changeColor = false;
+        }
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("changing color");
+            changeColor = true;
+
+        }
+    }
+}
